05ce252bc9c31db2647cc21c1f47b6322f9e64b0
Wang Haowei <hw_wang@whu.edu.cn>
2024-12-27 20:41:57 +0800

    config.py issues fixed

--- src/everyai/config/config.py
+++ src/everyai/config/config.py
@@ -1,16 +1,15 @@
 from pathlib import Path
-
+from everyai.everyai_path import GENERATE_CONFIG_PATH, DATA_LOAD_CONFIG_PATH, MONGO_CONFIG_PATH, BERT_TOPIC_CONFIG_PATH, DATA_PATH
 import yaml
 
 
 def get_config(file_path: Path):
-    if not file_path.is_file():
-        raise FileNotFoundError(f"The file {file_path} does not exist.")
-    if file_path.suffix != ".yaml":
-        raise ValueError("Only YAML files are supported.")
-
+    if file_path not in [
+        GENERATE_CONFIG_PATH,
+        DATA_LOAD_CONFIG_PATH,
+        MONGO_CONFIG_PATH,
+        BERT_TOPIC_CONFIG_PATH,
+    ]:
+        raise ValueError("Invalid config file path")
     with open(file_path, "r") as file:
-        try:
-            return yaml.safe_load(file)
-        except yaml.YAMLError as exc:
-            print(f"Error reading YAML file: {exc}")
+        return yaml.safe_load(file)
